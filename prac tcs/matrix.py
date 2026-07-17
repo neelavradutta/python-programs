2b5897d99093b757ec10b8c0128b3c193b47d82a
Neel <neelavra.dutta@gmail.com>
2026-07-17 14:22:02 +0530

    matrix

--- prac tcs/matrix.py
+++ prac tcs/matrix.py
@@ -1,10 +1,18 @@
-m=int(input())
+m,t=map(int,input().split())
 n=[]
 for i in range(m):
     n.append(list(map(int,input().split())))
 
+a=0
 for i in range(m):
-    for j in range(len(n)+1):
-        print(n[i][j], end=" ")
-    print()
+    count=0
+    for j in range(t):
+        if n[i][j]==1:
+           count=count+1
+    if count>a:
+        a=count
+        b=i
+
+        
+print(b)
     
