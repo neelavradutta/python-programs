9ba27392ac2cbcfea4b60267a4fd34bbc9e9bbd3
Neel <neelavra.dutta@gmail.com>
2026-07-28 22:40:33 +0530

    new

--- prac tcs/3452. Sum of Good Numbers.py
+++ prac tcs/3452. Sum of Good Numbers.py
@@ -4,11 +4,7 @@ k = int(input())
 s = 0
 
 for i in range(len(nums)):
-    if (0 <= i-k < len(nums)) and (0 <= i+k < len(nums)):
-        if nums[i] > nums[i-k] and nums[i] > nums[i+k]:
-            s += nums[i]
-
-    elif (i-k < 0) and (i+k >= len(nums)):
+    if (i-k < 0 or nums[i] > nums[i-k]) and (i+k >= len(nums) or nums[i] > nums[i+k]):
         s += nums[i]
 
 print(s)
---
+++ prac tcs/input.py
@@ -0,0 +1,2 @@
+s,f=map(str,input().split())
+print(s,f)
