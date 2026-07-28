687932bbb3e93b2b17b07e2bfd7ea0cc2a23c838
Neel <neelavra.dutta@gmail.com>
2026-07-29 03:16:39 +0530

    marathon

--- prac tcs/input.py
+++ prac tcs/input.py
@@ -1,2 +1,25 @@
-s,f=map(str,input().split())
-print(s,f)
+R = []
+
+while True:
+    x = input()
+
+    if x.lower() == "q":
+        break
+
+    d = float(x)
+
+    if d <= 0 or d > 42.195:
+        print("Invalid Input")
+        exit()
+
+    R.append(d)
+
+# Exclude finishers
+R = [i for i in R if i != 42.195]
+
+# Sort in descending order
+R.sort(reverse=True)
+
+# Print top 3 (or fewer)
+print("Highest Distance excluding Finishers:")
+print(R[:3])
