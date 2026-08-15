ed42222fb5b20fe5ead6c4dd5594989c03ad0d89
Neelavra Dutta <neelavra.dutta@gmail.com>
2026-08-15 19:49:53 +0530

    Update practice.py to replace previous string manipulation with list comparisons, checking the length of two item lists

--- prac tcs/practice.py
+++ prac tcs/practice.py
@@ -1,6 +1,3 @@
-a="2130"
-b=[]
-for i in range(len(a)):
-    for j in range(len(a)):
-        b.append(a[i:j])
-print(b)
+items1 = [[1,1],[4,5],[3,8]]
+items2 = [[3,1],[1,5]]
+print(len(items1)>len(items2))
