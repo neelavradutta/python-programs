3308e1bb74e05e788257431d8a0a4b0fe973c017
Neelavra Dutta <neelavra.dutta@gmail.com>
2026-08-31 00:12:17 +0530

    Update prac2.py to replace list operations with a new implementation that extracts a specific digit from a number, enhancing functionality and output clarity.

---
+++ prac tcs/1437. Check If All 1's .py
@@ -0,0 +1,5 @@
+logs = [[1993,1999],[2000,2010]]
+if 1993 in logs:
+    print(True)
+else:
+    print(False)
--- prac tcs/prac2.py
+++ prac tcs/prac2.py
@@ -1,5 +1,4 @@
-a=[[1,3],[1,8]]
-b=[1,4]
-print(a.index([1,8]))
-print(a[0])
-#3355
+nums=231
+wid=nums % 10
+y=str(nums)[wid:][0]
+print(y)
