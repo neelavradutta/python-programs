0b5730ab7981a887af42f41aee2a175b61845bdc
Neelavra Dutta <neelavra.dutta@gmail.com>
2026-09-01 14:36:23 +0530

    Add prime number check to 1437. Check If All 1's.py, replacing previous binary representation functionality with a new implementation that determines and prints if a number is prime or non-prime.

--- prac tcs/1437. Check If All 1's .py
+++ prac tcs/1437. Check If All 1's .py
@@ -1,3 +1,13 @@
+n=11
+Flag=True
+for i in range(2,n):
+    if n%i==0:
+        Flag=False
+        break
+if Flag==True:
+    print("prime")
+else:
+    print("Non prime")
 
-print(bin(14))
-print(bin(13))
+
+    
