cb07efa33c67eefe6c1ebb8623926ce1d53fb386
Neelavra Dutta <neelavra.dutta@gmail.com>
2026-09-07 16:51:56 +0530

    Enhance test.py to implement a frequency count of elements in a list and determine the minimum length of contiguous segments for the most frequent elements, improving functionality and output clarity.

--- prac tcs/test.py
+++ prac tcs/test.py
@@ -1,2 +1,25 @@
-s="fdd"
-t=s.copy
+nums = [1,2,2,3,1,4,2]
+a=[]
+for i in range(len(nums)):
+    a.append(nums.count(nums[i]))
+
+
+q=[]
+for i in range(len(nums)):
+    if nums.count(nums[i])==max(a):
+        q.append(nums[i])
+    
+
+print(list(set(q)))
+
+final=[]
+for i in range(len(q)):
+    d=[]
+    for j in range(len(nums)):
+        if q[i]==nums[j]:
+            d.append(j)
+    final.append(len(nums[min(d):max(d)+1]))
+
+print(min(final))
+
+
