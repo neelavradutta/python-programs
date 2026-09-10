46003c37c433113da543095074acad0ee76b6baf
Neelavra Dutta <neelavra.dutta@gmail.com>
2026-09-10 12:30:50 +0530

    Refactor prac5.py to update string manipulation logic, replacing the palindrome check with a new approach that prints the first half and second half of the string separately, enhancing output clarity.

--- prac tcs/prac5.py
+++ prac tcs/prac5.py
@@ -1,8 +1,10 @@
-a=[1,2,1]
-c=[]
-for i in range(len(a)-1,-1,-1):
-    c.append(a[i])
-if a==c:
-    print("pal")
-else:
-    print("non pal")
+a="123456"
+for i in range(len(a)//2):
+    print(a[i])
+
+print("gaaap")
+
+for i in range(len(a)//2,len(a)):
+    print(a[i])
+
+
