e5e477249b9b257ddfd490cdf7528ef855870817
Neelavra Dutta <neelavra.dutta@gmail.com>
2026-09-18 00:26:09 +0530

    Update check.py to modify the coordinates list and add a print statement for the sorted coordinates, enhancing output clarity and functionality.

--- prac tcs/check.py
+++ prac tcs/check.py
@@ -1,7 +1,10 @@
-coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[6,7]]
+
+coordinates = [[0,0],[0,1],[0,-1]]
 for i in range(len(coordinates)-1):
     if coordinates[i][0]+1==coordinates[i+1][0] or coordinates[i][0]==coordinates[i+1][0]:
         pass
     else:
         print("False")
         break
+
+print(sorted(coordinates))
