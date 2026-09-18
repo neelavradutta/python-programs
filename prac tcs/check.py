e0182a88c2e176de72b9df5fe5a402bff2c465c3
Neelavra Dutta <neelavra.dutta@gmail.com>
2026-09-19 00:17:58 +0530

    Refactor check.py to remove the coordinates evaluation and replace it with a print statement for the character 'a', simplifying the code and altering its output.

--- prac tcs/check.py
+++ prac tcs/check.py
@@ -1,10 +1,1 @@
-
-coordinates = [[0,0],[0,1],[0,-1]]
-for i in range(len(coordinates)-1):
-    if coordinates[i][0]+1==coordinates[i+1][0] or coordinates[i][0]==coordinates[i+1][0]:
-        pass
-    else:
-        print("False")
-        break
-
-print(sorted(coordinates))
+print(chr(97))
