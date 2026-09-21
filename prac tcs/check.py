658259f7a629d9f6b4a187c01bf0b8262b1b6143
Neelavra Dutta <neelavra.dutta@gmail.com>
2026-09-22 02:16:33 +0530

    Refactor check.py to implement a new logic that evaluates a list of coordinates, appending the distances between specific markers to a new list, enhancing functionality and output clarity.

--- prac tcs/check.py
+++ prac tcs/check.py
@@ -1,4 +1,9 @@
-for i in range(5):
-    for j in range(5):
-        print("*",end=" ")
-    print()
+forts = [1,0,0,-1,0,0,0,0,1]
+a=[]
+for i in range(len(forts)-1, 0, -1):
+    if forts[i] == -1 and 1 in forts[:i]:
+        for j in range(i-1, -1, -1):
+            if forts[j] == 1:
+                a.append(i-j-1)
+
+print(a)
