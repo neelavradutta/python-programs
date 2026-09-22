2ef4224e8e736c22a96ad2daf137ec87653fa1b3
Neelavra Dutta <neelavra.dutta@gmail.com>
2026-09-22 14:32:17 +0530

    Refactor check.py to replace the previous logic with a new implementation that calculates and prints the cube of a number, simplifying the code and altering its output.

--- prac tcs/check.py
+++ prac tcs/check.py
@@ -1,9 +1,3 @@
-forts = [1,0,0,-1,0,0,0,0,1]
-a=[]
-for i in range(len(forts)-1, 0, -1):
-    if forts[i] == -1 and 1 in forts[:i]:
-        for j in range(i-1, -1, -1):
-            if forts[j] == 1:
-                a.append(i-j-1)
-
-print(a)
+import math
+a=8
+print(math.pow(a,3))
