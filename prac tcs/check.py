ae0d2f3db9c1854063b72b6b12cf5037fc73167b
Neelavra Dutta <neelavra.dutta@gmail.com>
2026-09-24 02:12:02 +0530

    do this

--- prac tcs/check.py
+++ prac tcs/check.py
@@ -1,3 +1,5 @@
-dict={1:5,2:6,3:9,4:8}
-for i in range(1,len(dict)):
-    print(f"the value is {dict.get(i)}")
+dict={}
+for i in range(2):
+    dict[i]=int(input())
+
+print(f"the value is {dict.items()}")
