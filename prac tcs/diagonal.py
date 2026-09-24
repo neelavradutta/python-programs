af40b3d2179e4de94f18294b0e0726f7907d6dfe
Neelavra Dutta <neelavra.dutta@gmail.com>
2026-09-25 02:59:25 +0530

    Refactor diagonal.py to replace the previous logic with a nested loop that prints a grid of asterisks, simplifying the code and altering its output format.

--- prac tcs/diagonal.py
+++ prac tcs/diagonal.py
@@ -1,17 +1,4 @@
-a=[]
-b=[]
-nums = [[1,2,3,6],[5,6,7,7],[9,10,11,8],[4,8,2,3]]
-for i in range(len(nums)):
-    for j in range(len(nums[i])):
-        a.append(nums[i][i])
-        break
-
-print(nums[::-1])
-nums=nums[::-1]
-for i in range(len(nums)):
-    for j in range(len(nums[i])):
-        b.append(nums[i][i])
-        break
-
-print(a)
-print(b[::-1])
+for i in range(5):
+    for j in range(5):
+        print("* ",end="")
+    print()
